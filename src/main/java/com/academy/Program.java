e0384ed155903513cdbc3d68a7b53a68a5165719
https://github.com/Elmir675/Core_Java.git <ekorjenic10@gmail.com>
2025-11-23 20:33:46 +0100

    Izmijenjen Program.java i dodan ThreeDemo

--- src/main/java/com/academy/Program.java
+++ src/main/java/com/academy/Program.java
@@ -3,5 +3,6 @@ package com.academy;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Universe");
     }
 }
